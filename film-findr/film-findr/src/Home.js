4ba9cb5a304a1069dbc76b2ed5daaec83a767f2e
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2020-11-17 00:57:10 -0500

    Home page now renders popular movies

--- film-findr/film-findr/src/Home.js
+++ film-findr/film-findr/src/Home.js
@@ -1,96 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import MovieHomePage from "./MovieHomePage";
 import { animations } from "react-animation";
+import axios from "axios";
 
 function Home() {
   const style = {
     animation: animations.fadeIn,
   };
+
+  const [popularMoviesInfo, setPopularMoviesInfo] = useState([]);
+  const [genreList, setGenreList] = useState([]);
+  useEffect(() => {
+    async function fetchInfo() {
+      let response = await axios({
+        url: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_FIREBASE_imdb}&language=en-US&page=1`,
+        method: "GET",
+      });
+      setPopularMoviesInfo(response.data.results);
+
+      let genreResponse = await axios({
+        url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_FIREBASE_imdb}&language=en-US`,
+        method: "GET",
+      });
+      setGenreList(genreResponse.data.genres);
+    }
+    fetchInfo();
+  }, []);
+  console.log(popularMoviesInfo);
+  //.map((genre) => " " + genre.name)
+  //.toString();
   return (
     <div className="home">
       <div className="home__container">
         <img className="home__image" src="./lights.png" alt="" />
 
-        <div className="home__row" style={style}>
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-        </div>
-
-        <div className="home__row" style={style}>
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-        </div>
-
-        <div className="home__row" style={style}>
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
-          <MovieHomePage
-            title="Sample Movie"
-            image="https://images-na.ssl-images-amazon.com/images/I/71tUSFn3W0L._AC_SL1000_.jpg"
-            genres="Mystery, Supernatural, Action"
-            rating="9.0"
-          />
+        <div className="grid-container" style={style}>
+          {popularMoviesInfo.map(
+            ({ title, poster_path, genre_ids, vote_average }) => (
+              <MovieHomePage
+                className="grid-item"
+                title={title}
+                image={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                genres={genreList
+                  .filter((oneGenre) => genre_ids.includes(oneGenre.id))
+                  .map((one) => " " + one.name)
+                  .toString()}
+                rating={vote_average}
+              />
+            )
+          )}
         </div>
       </div>
     </div>
